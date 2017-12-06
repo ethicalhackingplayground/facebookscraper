a8cb828437f953efe0263d2d94e4766923102adf
Th3J0k3r <33673319+ethicalhackingplayground@users.noreply.github.com>
2017-12-06 02:06:10 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #
 # setup.py
 #
-# Gets everything prepped.
+# Gets everything ready.
 #
 
-
+sudo apt-get install python-setuptools python-dev build-essential
+sudo easy_install pip
+sudo pip install --upgrade virtualenv
 mkdir images
 sudo pip install -r requirements.txt
