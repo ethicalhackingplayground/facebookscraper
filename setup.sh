8676c5dfddef0515c13b91026fd3432c95823e3e
Th3J0k3r <33673319+ethicalhackingplayground@users.noreply.github.com>
2017-12-06 03:29:59 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,11 @@
 sudo apt-get install python-setuptools python-dev build-essential
 sudo easy_install pip
 sudo pip install --upgrade virtualenv
+wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux32.tar.gz
+tar -xvf geckodriver-v0.19.1-linux32.tar.gz
+sudo rm /usr/bin/geckodriver
+sudo cp geckodriver /usr/bin/
+rm geckodriver
+rm geckodriver-v0.19.1-linux32.tar.gz
 mkdir images
 sudo pip install -r requirements.txt
